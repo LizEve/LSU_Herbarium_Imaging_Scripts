d8bdda79f3254329075b5ec54318a3858e2fd2aa
Genevieve Mount <gmount1@lsu.edu>
2020-05-01 15:45:58 -0500

    tweaking

--- RunOrganize.sh
+++ RunOrganize.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # Path to your python script and output log to capture errors, even though you shouldn't need it. 
-python3 /mnt/c/Users/Image/Documents/WorkflowScriptsWS2/organizeIncomingImagesWS2.py &>> log.log
+python3 /mnt/c/Users/Image/Documents/WorkflowScriptsWS2/organizeIncomingImages.py &>> log.log
 # Add extra wait time, also probably unneeded 
 wait 60
