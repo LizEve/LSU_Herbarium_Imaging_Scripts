9db6ea5b14a38b866a8653b67a3235971437fb86
Genevieve Mount <gmount1@lsu.edu>
2020-06-30 10:36:40 -0500

    adding move csv files to rsync

--- rsyncDaily.sh
+++ rsyncDaily.sh
@@ -12,7 +12,6 @@ source1=/mnt/LSUCollectionsWS1/
 logfolder1=/mnt/LSUCollectionsWS1/Logs/
 outlog1=/mnt/LSUCollectionsWS1/Logs/rsync1.out # Only need one because rsync writes over it each time rsync is called
 suffix1=_serverWS1.txt # adding "_server" to end of log file so it is differentiated from log files sorted locally
-csvfolder1=/mnt/LSUCollectionsWS1/CSVLogs/
 # verbose and itemize changes, change ownership of moved files to match /data/
 # don't move CR2 files
 # pipe parses file paths that are moved 
@@ -58,3 +57,8 @@ done < $outlog2
 
 # Call script to change rsync log files to csv files ready for portal upload
 python3 /home/ggmount/WorkflowScripts/LogtoCSV.py &>> /data/LSUCollections/Logs/dailycsvlog.txt
+
+
+# Sync all csv files to sassafrass folder 
+csvRemote=/data/LSUCollections/Logs/
+rsync -avi -og --chown=root:adm --chmod=ug=rwx,o=r --update $csvfolder2 $csvRemote 
