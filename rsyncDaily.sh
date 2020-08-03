72594310e6196a9a03bd634f8d7f53cc9b0fed51
Genevieve Mount <gmount1@lsu.edu>
2020-08-03 15:44:09 -0500

    debugging rsync

--- rsyncDaily.sh
+++ rsyncDaily.sh
@@ -13,13 +13,15 @@ logfolder=/data/LSUCollections/Logs/
 outlog=/data/LSUCollections/Logs/rsync2.out # Only need one because rsync writes over it each time rsync is called
 suffix=_server_ws2.txt # adding "_server" to end of log file so it is differentiated from files sorted locally
 csvfolder=/mnt/LSUCollections/CSVLogs/
+fname=`date +'%Y-%m-%d'`
+todaylog=$logfolder2$fname$suffix2
 
 rsync -avi -og --chown=root:adm --chmod=ug=rwx,o=r --update --exclude '*CR2' --exclude '*Log*' $source $destination | grep '^>f' | cut -d' ' -f2 > $outlog
 
+
 while read g;
 do
-fname=`date +'%Y-%m-%d'`
-echo $g >> $logfolder$fname$suffix
+echo $g >> $todaylog
 mvdPath=$destination$g
 convert $mvdPath -units pixelsperinch -density 80x80 -resize 1400x1400^ -quality 80 ${mvdPath%.JPG}_WR.JPG
 convert $mvdPath -units pixelsperinch -density 80x80 -resize 200x200^ -quality 80 ${mvdPath%.JPG}_TN.JPG
@@ -32,5 +34,5 @@ rsync -avi -og --chown=root:adm --chmod=ug=rwx,o=r --update $csvfolder $csvRemot
 
 # checking if script is running
 echo $fname
-echo $logfolder$fname$suffix
+echo $todaylog
 
