0643e75625b43f45aa97ea4322ccfbc648ffdc68
Genevieve Mount <gmount1@lsu.edu>
2020-08-03 16:04:59 -0500

    debugging rsync

--- rsyncDaily.sh
+++ rsyncDaily.sh
@@ -5,14 +5,27 @@
 # outlogs should be output to source computer log folder
 # Source, destination, and logfolder MUST be folders WITH A TRAILING FORWARD SLASH
 
+
+
+# Set custom extension for log file names
+suffix=_server_ws2.txt # adding "_server" to end of log file so it is differentiated from files sorted locally
+
+# Set custom folders 
+# Where the image files are coming from and going to
+source=/mnt/LSUCollections/
 destination=/data/LSUCollections/
 
-# Repeat for second computer - WS2 
-source=/mnt/LSUCollections/
+# Where the log files are written to and the temporary daily log file
 logfolder=/data/LSUCollections/Logs/server_ws2_logs/
-outlog=/data/LSUCollections/Logs/server_ws2_logs/rsync2.out # Only need one because rsync writes over it each time rsync is called
-suffix=_server_ws2.txt # adding "_server" to end of log file so it is differentiated from files sorted locally
+outlog=/data/LSUCollections/Logs/server_ws2_logs/rsync2.out 
+
+# Where the log files get moved to for further counting and processing
+wsLogs=/mnt/LSUCollections/Logs/
+
+# Workstation and server csv folders for syncing 
 csvfolder=/mnt/LSUCollections/CSVLogs/
+csvRemote=/data/LSUCollections/Logs/
+
 
 # Name today's log after the date
 fname=`date +'%Y-%m-%d'`
@@ -29,6 +42,7 @@ date +"%T"
 
 while read g;
 do
+echo $g
 echo $g >> $todaylog
 mvdPath=$destination$g
 convert $mvdPath -units pixelsperinch -density 80x80 -resize 1400x1400^ -quality 80 ${mvdPath%.JPG}_WR.JPG
@@ -39,9 +53,11 @@ done < $outlog
 echo "starting log copy"
 date +"%T"
 
+# Copy logs to workstation 
+rsync -avi -og --chown=root:adm --chmod=ug=rwx,o=r --update $logfolder $wsLogs 
+
 
 # Sync all csv files to sassafrass folder 
-csvRemote=/data/LSUCollections/Logs/
 rsync -avi -og --chown=root:adm --chmod=ug=rwx,o=r --update $csvfolder $csvRemote 
 
 # checking if script is running
