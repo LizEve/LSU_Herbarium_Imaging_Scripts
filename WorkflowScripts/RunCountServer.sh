b09bc45449dcb663a78ffc14d79fb80562b3b075
Genevieve G. Mount <ggmountt@gmail.com>
2021-01-08 14:34:17 -0600

    Update RunCountServer.sh

--- WorkflowScripts/RunCountServer.sh
+++ WorkflowScripts/RunCountServer.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 # Set error log for debugging
-elog=/mnt/c/Users/Image/Documents/GitHub/LSU_Herbarium_Imaging_Scripts/WorkflowScriptsWS2/Debugging/CountServerLogs.log
+elog=/mnt/c/Users/Image/Documents/GitHub/LSU_Herbarium_Imaging_Scripts/WorkflowScripts/Debugging/CountServerLogs.log
 # Write date to error log 
 echo "$(date)" &>> $elog
 # Path to your python script 
-python3 /mnt/c/Users/Image/Documents/GitHub/LSU_Herbarium_Imaging_Scripts/WorkflowScriptsWS2/CountServerLogs.py &>> $elog
+python3 /mnt/c/Users/Image/Documents/GitHub/LSU_Herbarium_Imaging_Scripts/WorkflowScripts/CountServerLogs.py &>> $elog
 
 # Add extra wait time, a probably unneeded 
 sleep 5m
